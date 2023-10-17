9217aa26a6e19bf192388785053a21d01aaf17f4
Andrew <ahuang84@my.bcit.ca>
2023-10-16 23:37:29 -0700

    Finished main.js by chaining it with promises

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-const path = require("path");
 /*
- * Project: Milestone 1
- * File Name: main.js
- * Description:
- *
- * Created Date:
- * Author:
- *
- */
+* Project: Milestone 1
+* File Name: main.js
+* Description: The program takes a zipped file consisting of png images and 
+*              take each png image to grayscale. The grayscale image will then
+*              placeed inside a folder called grayscaled.
+* Created Date: Oct 10, 2023
+* Author: Andrew Huang | Set B
+*
+*/
 
 const IOhandler = require("./IOhandler");
+const path = require("path");
 const zipFilePath = path.join(__dirname, "myfile.zip");
 const pathUnzipped = path.join(__dirname, "unzipped");
 const pathProcessed = path.join(__dirname, "grayscaled");
 
 
-//Step 1: unzip myfile.zip
-// --> fs.createReadStream(zipFile)  | currently a stream
+IOhandler.unzip(zipFilePath, pathUnzipped)
+.then((data) => {
+    console.log(data)
+    return IOhandler.readDir(pathUnzipped)
+})
+.then((data) => {
+    const promisesArray = []
+    for(let i = 0; i < data.length; i++){
+        const imagePath = path.join(pathUnzipped, data[i]);
+        const imgOutputPath = path.join(pathProcessed, data[i]);
+        promises.push(IOhandler.grayScale(imagePath,imgOutputPath))
 
+    }
+    return promisesArray
+})
+.then((data) => {Promise.all(data)})
+.then(() => console.log("Process Completed!"))
+.catch((err) => console.log(err))
+
+
+//=============Instructors Notes=============I
 
 // Step 1: Read the zip file
 // Step 2: unzip the file
@@ -25,20 +44,9 @@ const pathProcessed = path.join(__dirname, "grayscaled");
 // Step 4: Send them to the grayscaled filter function
 // Step 5: After ALL IMAGES have SUCCESSFULLY been grayscaled then console.log("complete")
 
-//Read each png file...
-//--> fs.createReadStream(png1.png)
-
 //Use Promise.All()
-
-// fs.createReadStream(zipFilePath)
-// .pipe(unzipper.Extract({ path: pathUnzipped }));
 
 // IOHandler.unzip()
 //  .then(() => IOHandler.readDir())
 //  .then(() => IOHandler.grayScale())
 //  .catch(err => console.log(err))
-
-// IOhandler.unzip(zipFilePath, pathUnzipped)
-
-IOhandler.readDir(pathUnzipped)
-.then(data => console.log(data))
